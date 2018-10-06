c855cbb9b830636d4feba57844dfb1e70b324dab
Basel <basselhafiz@gmail.com>
2018-10-06 10:44:06 +0200

    Lesson 36 - Get device location

--- app/src/main/java/com/bmacode17/androideatit/activities/Cart.java
+++ app/src/main/java/com/bmacode17/androideatit/activities/Cart.java
@@ -1,9 +1,17 @@
 package com.bmacode17.androideatit.activities;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,10 +42,20 @@ import com.bmacode17.androideatit.models.Token;
 import com.bmacode17.androideatit.remotes.APIService;
 import com.bmacode17.androideatit.viewHolders.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -69,14 +87,13 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class Cart extends AppCompatActivity {
 
     private static final String TAG = "Basel";
-    private static final int PAYPAL_REQUEST_CODE = 1;
     public TextView textView_totalPrice;
     FirebaseDatabase database;
     DatabaseReference table_request;
     RecyclerView recyclerView_listCart;
     RecyclerView.LayoutManager layoutManager;
     EditText editText_notes;
-    //    EditText editText_address;
+//    EditText editText_address;
     FButton button_placeOrder;
     List<Order> carts = new ArrayList<>();
     CartAdapter cartAdapter;
@@ -86,16 +103,30 @@ public class Cart extends AppCompatActivity {
     String address, notes;
     Place shippingAddress;
 
-    // Press Ctrl + O
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+    private final int PLAY_SERVICES_RESOLUTION_REQUEST = 2;
+    private FusedLocationProviderClient mFusedLocationClient;
 
+    // Press Ctrl + O
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            if (checkPlayServices()) {
+                getDeviceLocation();
+            }
+        } else
+            checkLocationPermission();
 
         // Add this code before setContentView method
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
@@ -291,5 +322,104 @@ public class Cart extends AppCompatActivity {
         for (Order item : carts)
             new Database(this).addToCarts(item);
         loadCartList();
+    }
+
+    private boolean checkPlayServices() {
+
+        int resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+
+            if (GoogleApiAvailability.getInstance().isUserResolvableError(resultCode)) {
+
+                GoogleApiAvailability.getInstance().getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+
+            } else {
+
+                Toast.makeText(this, "Device isn't supported", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getDeviceLocation() {
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+
+            Task location = mFusedLocationClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful() && task.getResult() != null) {
+
+                        Location currentLocation = (Location) task.getResult();
+                        Log.d(TAG, "onComplete: Location: " + currentLocation.getLatitude() + " " + currentLocation.getLongitude());
+                        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                        Toast.makeText(Cart.this, currentLocation.getLatitude() + " , " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(Cart.this, "Unable to get current location !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (SecurityException ex) {
+            Log.d(TAG, "GetDeviceLocation : SecurityException: " + ex.getMessage());
+        }
+    }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(Cart.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayServices()) {
+                        getDeviceLocation();
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 }
